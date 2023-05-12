e923a365760821640e8943c73e5efd20b74d7057
saci zakaria <sacideveloppeur@gmail.com>
2023-05-12 09:35:00 +0200

    CircleAvatar / Text(                                  "user Name"

--- lib/widgets/my_drawer.dart
+++ lib/widgets/my_drawer.dart
@@ -16,7 +16,25 @@ class _MyDrawerState extends State<MyDrawer> {
       child: ListView(
         children: [
           //header
-          Container(),
+          Container(
+            child: const Column(
+              children: [
+                //user profile image
+                CircleAvatar(
+                  backgroundImage: NetworkImage( "https://images.unsplash.com/photo-1446707052533-0e1d48e08aa9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dHJlc3N8ZW58MHx8MHx8&w=1000&q=80"),
+                ),
+                //user name
+                Text(
+                  "user Name",
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
           //body
           Container(),
         ],
