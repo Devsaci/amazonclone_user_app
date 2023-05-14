7f828036f324699e0e2d208c9cd099cc12e07552
saci zakaria <sacideveloppeur@gmail.com>
2023-05-14 19:42:52 +0200

    bottom: const TabBar

--- lib/authScreens/auth_screen.dart
+++ lib/authScreens/auth_screen.dart
@@ -11,24 +11,22 @@ class AuthScreen extends StatefulWidget {
 class _AuthScreenState extends State<AuthScreen> {
   @override
   Widget build(BuildContext context) {
-    return  DefaultTabController(
+    return DefaultTabController(
       length: 2,
       child: Scaffold(
         appBar: AppBar(
           flexibleSpace: Container(
-            decoration:  BoxDecoration(
+            decoration: BoxDecoration(
                 gradient: LinearGradient(
-                  colors:
-                  [
-                    Colors.amber.shade500,
-                    Colors.amber.shade900,
-                  ],
-                  begin: const FractionalOffset(0.0, 0.0),
-                  end: const FractionalOffset(1.0, 0.0),
-                  stops: const [0.0, 1.0],
-                  tileMode: TileMode.clamp,
-                )
-            ),
+              colors: [
+                Colors.amber.shade500,
+                Colors.amber.shade900,
+              ],
+              begin: const FractionalOffset(0.0, 0.0),
+              end: const FractionalOffset(1.0, 0.0),
+              stops: const [0.0, 1.0],
+              tileMode: TileMode.clamp,
+            )),
           ),
           title: const Text(
             "iShop",
@@ -39,6 +37,23 @@ class _AuthScreenState extends State<AuthScreen> {
             ),
           ),
           centerTitle: true,
+          bottom: const TabBar(
+            indicatorColor: Colors.black87,
+            tabs: [
+              Tab(
+                icon: Icon(
+                  Icons.lock,
+                  color: Colors.white,
+                ),
+              ),
+              Tab(
+                icon: Icon(
+                  Icons.person,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
