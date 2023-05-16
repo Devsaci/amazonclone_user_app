e92624b3769ea812200287f941df1beb8aed5669
saci zakaria <sacideveloppeur@gmail.com>
2023-05-16 21:44:56 +0200

    Image.asset / ElevatedButton

--- lib/authScreens/login_tab_page.dart
+++ lib/authScreens/login_tab_page.dart
@@ -10,34 +10,43 @@ class _LoginTabPageState extends State<LoginTabPage> {
   TextEditingController emailTextEditingController = TextEditingController();
   TextEditingController passwordTextEditingController = TextEditingController();
 
-  GlobalKey<FormState>  formKey = GlobalKey<FormState>();
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Form(
-        key: formKey,
-        child: Column(
-          children: [
-            //email
-            CustomTextField(
-              textEditingController: emailTextEditingController,
-              isObsecre: false,
-              iconData: Icons.email,
-              hintText: "Email",
-              enabled: true,
+      child: Column(
+        children: [
+          Image.asset("name"),
+          Form(
+            key: formKey,
+            child: Column(
+              children: [
+                //email
+                CustomTextField(
+                  textEditingController: emailTextEditingController,
+                  isObsecre: false,
+                  iconData: Icons.email,
+                  hintText: "Email",
+                  enabled: true,
+                ),
+                const SizedBox(height: 10),
+                //pass
+                CustomTextField(
+                  textEditingController: passwordTextEditingController,
+                  isObsecre: true,
+                  iconData: Icons.lock,
+                  hintText: "Password",
+                  enabled: true,
+                ),
+              ],
             ),
-            const SizedBox(height: 10),
-            //pass
-            CustomTextField(
-              textEditingController: passwordTextEditingController,
-              isObsecre: true,
-              iconData: Icons.lock,
-              hintText: "Password",
-              enabled: true,
-            ),
-          ],
-        ),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text("data"),
+          ),
+        ],
       ),
     );
   }
