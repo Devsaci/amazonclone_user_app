8edfda0e5d355b929755eebc8eee5437875adbd8
saci zakaria <sacideveloppeur@gmail.com>
2023-05-17 19:18:58 +0200

    CircleAvatar Icons.add_photo_alternate

--- lib/authScreens/registration_tab_page.dart
+++ lib/authScreens/registration_tab_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class RegistrationTabPage extends StatefulWidget {
   const RegistrationTabPage({Key? key}) : super(key: key);
@@ -12,8 +13,13 @@ class _RegistrationTabPageState extends State<RegistrationTabPage> {
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Container(
-        child: Column(
-          children: [],
+        child: const Column(
+          children: [
+            SizedBox(height: 12),
+            //get-capture image
+            CircleAvatar(child: Icon(Icons.add_photo_alternate)),
+            //inputs form fields
+          ],
         ),
       ),
     );
