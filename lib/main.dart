1131603ef69a7d2e0182093736801441d2fcc39d
saci zakaria <sacideveloppeur@gmail.com>
2023-05-21 21:34:58 +0200

    await SharedPreferences.getInstance();

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,20 @@ import 'package:amazonclone_user_app/mainScreens/home_screen.dart';
 import 'package:amazonclone_user_app/splashScreen/my_splash_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'authScreens/auth_screen.dart';
+import 'global/global.dart';
 
-Future <void> main() async
-{
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  sharedPreferences = await SharedPreferences.getInstance();
   await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
 
   @override
   Widget build(BuildContext context) {
