f889d88f80eeaa074ae60ee7ab5ee0aa7d8005ba
saci zakaria <sacideveloppeur@gmail.com>
2023-05-22 21:41:29 +0200

    doc(currentUser.uid)

--- lib/authScreens/login_tab_page.dart
+++ lib/authScreens/login_tab_page.dart
@@ -60,7 +60,7 @@ class _LoginTabPageState extends State<LoginTabPage> {
 
   }
   void checkIfUserRecordExists(User currentUser) async{
-    await FirebaseFirestore.instance.collection("collectionPath");
+    await FirebaseFirestore.instance.collection("users").doc(currentUser.uid);
   }
 
   @override
