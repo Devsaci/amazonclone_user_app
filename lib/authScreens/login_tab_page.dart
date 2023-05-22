97582c18a27ec73a8bfacb495d30abf3f5852d52
saci zakaria <sacideveloppeur@gmail.com>
2023-05-22 22:22:33 +0200

    sharedPreferences!.setString("name", record.data()!["name"]);

--- lib/authScreens/login_tab_page.dart
+++ lib/authScreens/login_tab_page.dart
@@ -73,6 +73,7 @@ class _LoginTabPageState extends State<LoginTabPage> {
         {
           await sharedPreferences!.setString("uid", record.data()!["uid"]);
           await sharedPreferences!.setString("email", record.data()!["email"]);
+          await sharedPreferences!.setString("name", record.data()!["name"]);
         }
         else
         {
