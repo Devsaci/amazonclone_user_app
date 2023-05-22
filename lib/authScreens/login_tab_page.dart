11c46a43d482077b885539d315aa1de400494730
saci zakaria <sacideveloppeur@gmail.com>
2023-05-22 22:23:52 +0200

    //send user to home screen

--- lib/authScreens/login_tab_page.dart
+++ lib/authScreens/login_tab_page.dart
@@ -75,6 +75,7 @@ class _LoginTabPageState extends State<LoginTabPage> {
           await sharedPreferences!.setString("email", record.data()!["email"]);
           await sharedPreferences!.setString("name", record.data()!["name"]);
           await sharedPreferences!.setString("photoUrl", record.data()!["photoUrl"]);
+          //send user to home screen
         }
         else
         {
