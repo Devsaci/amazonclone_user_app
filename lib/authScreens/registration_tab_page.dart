a31909c69aa1a62414cad00b4603c0166319f5a6
saci zakaria <sacideveloppeur@gmail.com>
2023-05-22 20:21:49 +0200

    "$message, Please wait...",

--- lib/authScreens/registration_tab_page.dart
+++ lib/authScreens/registration_tab_page.dart
@@ -13,31 +13,24 @@ import '../splashScreen/my_splash_screen.dart';
 import '../widgets/custom_text_field.dart';
 import '../widgets/loading_dialog.dart';
 
-class RegistrationTabPage extends StatefulWidget
-{
+class RegistrationTabPage extends StatefulWidget {
   @override
   State<RegistrationTabPage> createState() => _RegistrationTabPageState();
 }
 
-
-
-class _RegistrationTabPageState extends State<RegistrationTabPage>
-{
+class _RegistrationTabPageState extends State<RegistrationTabPage> {
   TextEditingController nameTextEditingController = TextEditingController();
   TextEditingController emailTextEditingController = TextEditingController();
   TextEditingController passwordTextEditingController = TextEditingController();
-  TextEditingController confirmPasswordTextEditingController = TextEditingController();
+  TextEditingController confirmPasswordTextEditingController =
+      TextEditingController();
   GlobalKey<FormState> formKey = GlobalKey<FormState>();
   String downloadUrlImage = "";
 
   XFile? imgXFile;
   final ImagePicker imagePicker = ImagePicker();
 
-
-
-
-  getImageFromGallery() async
-  {
+  getImageFromGallery() async {
     imgXFile = await imagePicker.pickImage(source: ImageSource.gallery);
 
     setState(() {
@@ -45,157 +38,151 @@ class _RegistrationTabPageState extends State<RegistrationTabPage>
     });
   }
 
-  formValidation() async
-  {
-    if(imgXFile == null) //image is not selected
-        {
+  formValidation() async {
+    if (imgXFile == null) //image is not selected
+    {
       Fluttertoast.showToast(msg: "Please select an image.");
-    }
-    else //image is already selected
-        {
+    } else //image is already selected
+    {
       //password is equal to confirm password
-      if(passwordTextEditingController.text == confirmPasswordTextEditingController.text)
-      {
+      if (passwordTextEditingController.text ==
+          confirmPasswordTextEditingController.text) {
         //check email, pass, confirm password & name text fields
-        if(nameTextEditingController.text.isNotEmpty
-            && emailTextEditingController.text.isNotEmpty
-            && passwordTextEditingController.text.isNotEmpty
-            && confirmPasswordTextEditingController.text.isNotEmpty)
-        {
+        if (nameTextEditingController.text.isNotEmpty &&
+            emailTextEditingController.text.isNotEmpty &&
+            passwordTextEditingController.text.isNotEmpty &&
+            confirmPasswordTextEditingController.text.isNotEmpty) {
           showDialog(
               context: context,
-              builder: (c)
-              {
+              builder: (c) {
                 return const LoadingDialogWidget(
                   message: "Registering your account",
                 );
-              }
-          );
+              });
 
           //1.upload image to storage
           String fileName = DateTime.now().millisecondsSinceEpoch.toString();
 
           fStorage.Reference storageRef = fStorage.FirebaseStorage.instance
               .ref()
-              .child("usersImages").child(fileName);
+              .child("usersImages")
+              .child(fileName);
 
-          fStorage.UploadTask uploadImageTask = storageRef.putFile(File(imgXFile!.path));
+          fStorage.UploadTask uploadImageTask =
+              storageRef.putFile(File(imgXFile!.path));
 
-          fStorage.TaskSnapshot taskSnapshot = await uploadImageTask.whenComplete(() {});
+          fStorage.TaskSnapshot taskSnapshot =
+              await uploadImageTask.whenComplete(() {});
 
-          await taskSnapshot.ref.getDownloadURL().then((urlImage)
-          {
+          await taskSnapshot.ref.getDownloadURL().then((urlImage) {
             downloadUrlImage = urlImage;
           });
 
           //2. save the user info to firestore database
           saveInformationToDatabase();
-        }
-        else
-        {
+        } else {
           Navigator.pop(context);
-          Fluttertoast.showToast(msg: "Please complete the form. Do not leave any text field empty.");
+          Fluttertoast.showToast(
+              msg:
+                  "Please complete the form. Do not leave any text field empty.");
         }
-      }
-      else //password is NOT equal to confirm password
-          {
-        Fluttertoast.showToast(msg: "Password and Confirm Password do not match.");
+      } else //password is NOT equal to confirm password
+      {
+        Fluttertoast.showToast(
+            msg: "Password and Confirm Password do not match.");
       }
     }
   }
 
-  saveInformationToDatabase() async
-  {
+  saveInformationToDatabase() async {
     //authenticate the user first
     User? currentUser;
 
-    await FirebaseAuth.instance.createUserWithEmailAndPassword(
+    await FirebaseAuth.instance
+        .createUserWithEmailAndPassword(
       email: emailTextEditingController.text.trim(),
       password: passwordTextEditingController.text.trim(),
-    ).then((auth)
-    {
+    )
+        .then((auth) {
       currentUser = auth.user;
-    }).catchError((errorMessage)
-    {
+    }).catchError((errorMessage) {
       Navigator.pop(context);
       Fluttertoast.showToast(msg: "Error Occurred: \n $errorMessage");
     });
 
-    if(currentUser != null)
-    {
+    if (currentUser != null) {
       //save info to database and save locally
       saveInfoToFirestoreAndLocally(currentUser!);
     }
   }
 
-  saveInfoToFirestoreAndLocally(User currentUser) async
-  {
+  saveInfoToFirestoreAndLocally(User currentUser) async {
     //save to firestore
-    FirebaseFirestore.instance
-        .collection("users")
-        .doc(currentUser.uid)
-        .set(
-        {
-          "uid": currentUser.uid,
-          "email": currentUser.email,
-          "name": nameTextEditingController.text.trim(),
-          "photoUrl": downloadUrlImage,
-          "status": "approved",
-          "userCart": ["initialValue"],
-        });
+    FirebaseFirestore.instance.collection("users").doc(currentUser.uid).set({
+      "uid": currentUser.uid,
+      "email": currentUser.email,
+      "name": nameTextEditingController.text.trim(),
+      "photoUrl": downloadUrlImage,
+      "status": "approved",
+      "userCart": ["initialValue"],
+    });
 
     //save locally
     sharedPreferences = await SharedPreferences.getInstance();
     await sharedPreferences!.setString("uid", currentUser.uid);
     await sharedPreferences!.setString("email", currentUser.email!);
-    await sharedPreferences!.setString("name", nameTextEditingController.text.trim());
+    await sharedPreferences!
+        .setString("name", nameTextEditingController.text.trim());
     await sharedPreferences!.setString("photoUrl", downloadUrlImage);
     await sharedPreferences!.setStringList("userCart", ["initialValue"]);
 
-    Navigator.push(context, MaterialPageRoute(builder: (c)=> const MySplashScreen()));
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (c) =>  const MySplashScreen(),
+      ),
+    );
   }
 
   @override
-  Widget build(BuildContext context)
-  {
+  Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Container(
         child: Column(
           children: [
-
-            const SizedBox(height: 12,),
+            const SizedBox(
+              height: 12,
+            ),
 
             //get-capture image
             GestureDetector(
-              onTap: ()
-              {
+              onTap: () {
                 getImageFromGallery();
               },
               child: CircleAvatar(
                 radius: MediaQuery.of(context).size.width * 0.20,
                 backgroundColor: Colors.white,
-                backgroundImage: imgXFile == null
-                    ? null
-                    : FileImage(
-                    File(imgXFile!.path)
-                ),
+                backgroundImage:
+                    imgXFile == null ? null : FileImage(File(imgXFile!.path)),
                 child: imgXFile == null
                     ? Icon(
-                  Icons.add_photo_alternate,
-                  color: Colors.grey,
-                  size: MediaQuery.of(context).size.width * 0.20,
-                ) : null,
+                        Icons.add_photo_alternate,
+                        color: Colors.grey,
+                        size: MediaQuery.of(context).size.width * 0.20,
+                      )
+                    : null,
               ),
             ),
 
-            const SizedBox(height: 12,),
+            const SizedBox(
+              height: 12,
+            ),
 
             //inputs form fields
             Form(
               key: formKey,
               child: Column(
                 children: [
-
                   //name
                   CustomTextField(
                     textEditingController: nameTextEditingController,
@@ -232,19 +219,20 @@ class _RegistrationTabPageState extends State<RegistrationTabPage>
                     enabled: true,
                   ),
 
-                  const SizedBox(height: 20,),
-
+                  const SizedBox(
+                    height: 20,
+                  ),
                 ],
               ),
             ),
 
             ElevatedButton(
                 style: ElevatedButton.styleFrom(
-                  primary: Colors.pinkAccent,
-                  padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 12),
+                  backgroundColor: Colors.pinkAccent,
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 50, vertical: 12),
                 ),
-                onPressed: ()
-                {
+                onPressed: () {
                   formValidation();
                 },
                 child: const Text(
@@ -253,11 +241,11 @@ class _RegistrationTabPageState extends State<RegistrationTabPage>
                     color: Colors.white,
                     fontWeight: FontWeight.bold,
                   ),
-                )
+                )),
+
+            const SizedBox(
+              height: 30,
             ),
-
-            const SizedBox(height: 30,),
-
           ],
         ),
       ),
